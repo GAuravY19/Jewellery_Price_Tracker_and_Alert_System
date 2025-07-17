169602de1c298c4467275d507a4fa16a4e24aedc
gaurav <gauravy1905@gmail.com>
2025-07-17 09:39:47 +0530

    feat: completed the scrolling logic for bluestone page(infinite scrolling page)

--- scrappers/bluestone.py
+++ scrappers/bluestone.py
@@ -1,78 +1,51 @@
 from playwright.sync_api import sync_playwright
 import time
 
-pw = sync_playwright().start()
-browser = pw.chromium.launch(headless=False)
-
-page = browser.new_page()
-page.goto("https://www.bluestone.com/")
-page.wait_for_load_state()
-
-page.locator('.deny-btn').click()
-
-products = ['Rings', 'Earrings', 'Solitaires', 'Nosepins', 'Bracelets', 'Bangles',
+PRODUCTS = ['Rings', 'Earrings', 'Solitaires', 'Nosepins', 'Bracelets', 'Bangles',
             'Necklace', 'Pendants', 'Mangalsutras']
 
-page.locator('xpath=//input[@id="search_query_top_elastic_search"]').fill(products[0])
+PW = sync_playwright().start()
+BROWSER = PW.chromium.launch(headless=False)
+
+PAGE = BROWSER.new_page()
+PAGE.goto("https://www.bluestone.com/")
+PAGE.wait_for_load_state()
+
+PAGE.locator('.deny-btn').click()
+PAGE.locator('xpath=//input[@id="search_query_top_elastic_search"]').fill(PRODUCTS[0])
+
 time.sleep(3)
-page.locator('.button').click()
+
+PAGE.locator('.button').click()
 
 time.sleep(5)
+
+total_designs = PAGE.locator("xpath =//span[@class='total-designs']").text_content().split(" ")[0]
+total_designs = int(total_designs)
+print(total_designs)
+
 prod = []
 
-print("INside page scrolling page")
-i = 0
-data_page = 0
+i = 1
 while i != 50:
-    page.mouse.wheel(0,90)
-    i+=1
-    time.sleep(1)
-    try:
-        list_of_prices = page.locator(f'xpath=//li[@data-position="{i}"]//h2[@class="p-name"]')
-        print(list_of_prices.text_content())
-    except:
-        break
+    PAGE.mouse.wheel(0,200)
+    print(i)
+    i += 1
 
-print("outside of page scrolling page")
+time.sleep(3)
 
+ui_elements = PAGE.locator("#product_list_ui li").all()
 
-# time.sleep(5)
+count = len(ui_elements)
 
-# for i in list_of_prices:
-#     prod = i.text_content()
-# print(len(prod))
-# print(list_of_prices.text_content())
+print('Total products found : ', count)
+for i in ui_elements:
+    selling_price = i.get_attribute("data-sellingprice")
+    prod_name = i.get_attribute('data-pname')
+    print(selling_price)
+    break
 
 print('Done All Jewellery')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-time.sleep(100)
-browser.close()
+time.sleep(180)
+BROWSER.close()
