7c2f3401de1f146872ab0ab2d95392215cf46d08
gaurav <gauravy1905@gmail.com>
2025-07-23 13:39:49 +0530

    Forms page

--- flaskApp/forms.py
+++ flaskApp/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flaskApp.models import User
 
 class RegistrationForm(FlaskForm):
     Username = StringField('Username: ',
@@ -15,6 +16,19 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password: ', validators=[DataRequired(), EqualTo('password')])
 
     submit = SubmitField('Sign Up')
+
+
+    def validate_username(self, Username):
+        user = User.query.filter_by(username = Username.data).first()
+
+        if user:
+            raise ValidationError('Username Already exists')
+
+    def validate_email(self, email):
+        email = User.query.filter_by(email = email.data)
+
+        if email:
+            raise ValidationError('Email Already exists')
 
 
 class LoginForm(FlaskForm):
