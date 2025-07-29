c177906edb272cb37b6a70db7e84fa989a3deebe
gaurav <gauravy1905@gmail.com>
2025-07-29 12:31:35 +0530

    Updated Edit profile form

--- flaskApp/forms.py
+++ flaskApp/forms.py
@@ -2,19 +2,16 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskApp.models import User
+from flask_login import current_user
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username: ',
                            validators=[DataRequired(), Length(min=6, max=20)])
-
     email = StringField('Email: ',
                         validators=[DataRequired(), Email()])
-
     password = PasswordField('Password: ',
                              validators=[DataRequired(), Length(min=10)])
-
     confirm_password = PasswordField('Confirm Password: ', validators=[DataRequired(), EqualTo('password')])
-
     submit = SubmitField('Sign Up')
 
     def validate_username(self, username):
@@ -23,6 +20,7 @@ class RegistrationForm(FlaskForm):
         if user:
             print("Validation Error")
             raise ValidationError('Username already exists!')
+
 
     def validate_email(self, email):
         check_email = User.query.filter_by(email = email.data).first()
@@ -34,24 +32,30 @@ class RegistrationForm(FlaskForm):
 class LoginForm(FlaskForm):
     email = StringField('Email: ',
                         validators=[DataRequired(), Email()])
-
     password = PasswordField('Password: ',
                              validators=[DataRequired(), Length(min=10)])
-
     remember = BooleanField('Remember Me')
-
     submit = SubmitField('Sign In')
 
 
 class EditForm(FlaskForm):
-    Username = StringField('Username: ',
+    username = StringField('Username: ',
                            validators=[DataRequired(), Length(min=6, max=20)])
-
     email = StringField('Email: ',
                         validators=[DataRequired(), Email()])
-
-    # password = PasswordField('Password: ',
-    #                          validators=[DataRequired(), Length(min=10)])
-
     submit = SubmitField('Update Profile')
 
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username = username.data).first()
+
+            if user:
+                raise ValidationError('Username already exists')
+
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            email = User.query.filter_by(email = email.data).first()
+
+            if email:
+                raise ValidationError('Email Already exists')
