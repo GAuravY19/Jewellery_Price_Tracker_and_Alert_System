8434a4783c004858cc70166ee2a9fe554e3cc043
gaurav <gauravy1905@gmail.com>
2025-07-31 13:40:59 +0530

    Updated dotenv

--- flaskApp/__init__.py
+++ flaskApp/__init__.py
@@ -2,10 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '2116ecc90bebada37c62ff7655a46b52dc58f364ae36948f6849d61307dd8cbb781656ff6322c4f918ad09d63dfcec21366cef777bf287be4a1772368d16b33f'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
